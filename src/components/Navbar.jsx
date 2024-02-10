548eb8ab925d15d3dcbd45c66e9f47b7a84aa025
kartikeyasureka <kartikeyasureka@gmail.com>
2024-02-10 16:00:05 +0530

    tailwind added and first component created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Reach from 'react'
+
+const Navbar = () => {
+    return(
+        <div className='text-white'>
+            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        </div>
+    )
+}
+
+export default Navbar
