7b8b585a0f77725f092bc9c0064e9fc09f9ebb1e
kartikeyasureka <kartikeyasureka@gmail.com>
2024-02-11 14:52:09 +0530

    navbar button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,12 @@ const Navbar = () => {
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
             <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
             <ul className='hidden md:flex'>
-                <li className='p-4'>Home</li>
-                <li className='p-4'>Company</li>
-                <li className='p-4'>Resources</li>
-                <li className='p-4'>About</li>
-                <li className='p-4'>Contact</li>
+                <li className='p-4 pt-7'>Home</li>
+                <li className='p-4 pt-7'>Company</li>
+                <li className='p-4 pt-7'>Resources</li>
+                <li className='p-4 pt-7'>About</li>
+                <li className='p-4 pt-7'>Contact</li>
+                <li className='p-4 pl-0'><button className='text-black bg-[#00df9a] px-2 py-4 w-[100px] text-xs rounded-md md:flex hidden justify-center'>Get Started</button></li>
             </ul>
             <div onClick={handleNav} className='block md:hidden'>
                 {nav ? <AiOutlineClose size = {20} /> : <AiOutlineMenu size={20}/>}
